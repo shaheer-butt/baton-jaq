4650d3e26992fa91be9c9f1f425dda615de23ed9
shaheer-butt <infoabdullahsh@gmail.com>
2026-06-12 20:36:29 +0500

    Complete Homepgae Except Header Responsiveness

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ window.addEventListener('load', function () {
     type: 'loop',
 
     autoWidth: true,
-    autoplay: true,
+    autoplay: false,
     interval: 3000,
     speed: 600,
     easing: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
