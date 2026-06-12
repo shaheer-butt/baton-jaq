0cc5e2ab5b2b06640ee9bad3af67158f219b3e67
shaheer-butt <infoabdullahsh@gmail.com>
2026-06-12 11:50:45 +0500

    Updated

--- js/main.js
+++ js/main.js
@@ -26,3 +26,33 @@ window.addEventListener('load', function () {
     splide.go('>');
   });
 });
+
+
+/* ===========================================
+          Splide Js Project  Slider 
+=========================================== */
+window.addEventListener('load', function () {
+  var splide = new Splide('.project-slider', {
+    type: 'loop',
+
+    autoWidth: true,
+    autoplay: true,
+    interval: 3000,
+    speed: 600,
+    easing: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
+    pauseOnHover: true,
+    arrows: false,
+    pagination: false,
+    gap: 24,
+    padding: 0,
+    drag: true,
+    trimSpace: false,
+  }).mount();
+
+  document.querySelector('.project-slider-prev-arrow').addEventListener('click', function () {
+    splide.go('<');
+  });
+  document.querySelector('.project-slider-next-arrow').addEventListener('click', function () {
+    splide.go('>');
+  });
+});
